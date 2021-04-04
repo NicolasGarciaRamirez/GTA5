7b586b85e534c2ece93c447aa528219c04faca25
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-04-04 12:16:13 -0500

    update

--- resources/[local]/mymode/mymode_client.js
+++ resources/[local]/mymode/mymode_client.js
@@ -1,4 +1,4 @@
-const spawnPos = [266.4990234375, 6631, 29.55451011657715];
+const spawnPos = [394.8529, -730.8, 26.3985];
 
 
 on('onClientGameTypeStart', () => {
@@ -33,7 +33,7 @@ RegisterCommand('cords', function(){
     notify(`Your Position Is: x =${cords[0]}, y =${cords[1]} , z =${cords[2]}`)
 })
 
-RegisterCommand('tp', async(cords) => {
+RegisterCommand('tp', async([cords]) => {
   exports.spawnmanager.setAutoSpawnCallback(() => {
     exports.spawnmanager.spawnPlayer({
       x: cords[0],
@@ -47,5 +47,18 @@ RegisterCommand('tp', async(cords) => {
 
   exports.spawnmanager.setAutoSpawn(true)
   exports.spawnmanager.forceRespawn()
+})
+
+let cars = ["adder", "comet", "cheetah", "faggio"]
+
+RegisterCommand("car", function(){
+    let car = (cars[Math.random(cars.length)])
+    spawnCar(car)
+    notify("~p~Spawned car: ~h~~g~" + car)
+})
+
+RegisterCommand("die", function(){
+    SetEntityHealth(PlayerPedId(), 0)
+    notify("~r~ You Died.")
 })
 
